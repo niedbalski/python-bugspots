b99eae99ae3028eb62f04390063c371b8276d023
Punit Goswami <punit.goswami@wingify.com>
2021-07-17 11:59:02 +0530

    changes in setup

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import setup, find_packages
 dependencies = [ "vcstools-latest" ]
 
 setup(
-    name="bug-spots",
-    version="0.3",
+    name="bug-hot-spots",
+    version="0.1",
     packages=find_packages(),
     install_requires=dependencies,
-    author="Jorge Niedbalski R.",
-    author_email="jnr@pyrosome.org",
+    author="Punit Goswami",
+    author_email="punit.goswami@wingify.com",
     description="A Python based implementation of the bug prediction algorithm proposed by Google",
-    keywords="bugspot bug spots bug prediction",
+    keywords="bughotspot bug spots bug prediction",
     include_package_data=True,
     license="BSD",
     entry_points={
 	'console_scripts' : [
-		'bugspots = bugspots:main'
+		'bughotspots = bughotspots:main'
 	]
     },
 
-    classifiers=['Development Status :: 3 - Alpha',
+    classifiers=['Development Status :: 1 - Alpha',
                 'Intended Audience :: Developers',
                 'Operating System :: Unix ']
 )
