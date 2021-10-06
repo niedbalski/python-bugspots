800a149df4245ce7b8ab17a202f9298df8a4bfdb
Punit Goswami <pntgoswami18@gmail.com>
2021-10-06 20:44:49 +0530

    added longDescription, proejct URL

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 dependencies = [ "vcstools-latest" ]
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="bug-hot-spots",
     version="0.1",
@@ -10,6 +13,9 @@ setup(
     author="Punit Goswami",
     description="A Python based implementation of the bug prediction algorithm proposed by Google",
     keywords="bughotspot bug spots bug prediction",
+    long_description=long_description,
+    url='https://github.com/pntgoswami18/python-bugspots',
+    long_description_content_type="text/markdown",
     include_package_data=True,
     license="BSD",
     entry_points={
